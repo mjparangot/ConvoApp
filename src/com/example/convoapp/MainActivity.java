c34dddde56ed03744c6ad6ed51695beb49af3cc7
Matthew Parangot <mjparangot@lawn-143-215-206-119.lawn.gatech.edu>
2014-11-21 21:41:00 -0500

    Material stuff

--- src/com/example/convoapp/MainActivity.java
+++ src/com/example/convoapp/MainActivity.java
@@ -4,12 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// Remove title bar
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); 
+		
 		setContentView(R.layout.activity_main);
 	}
 }
